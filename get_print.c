2ac51fc4d4426c0347c06c4f4a167a7957b74d5d
lroudge <lauradrc@hotmail.fr>
2019-03-18 02:21:39 +0000

    Removed other structs from get_print

--- get_print.c
+++ get_print.c
@@ -13,20 +13,13 @@
 int (*get_print(char s))(va_list)
 {
 	ph func_arr[] = {
-		{'i', print_int},
 		{'s', print_string},
 		{'c', print_char},
-		{'d', print_int},
-		{'u', print_unsigned},
-		{'x', print_hex},
-		{'X', print_hex_big},
-		{'b', print_binary},
-		{'o', print_octal}
 		};
 
 	register int i;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i < 2; i++)
 		if (func_arr[i].c == s)
 			return (func_arr[i].f);
 	return (NULL);
