db81731115fc0e756af8718cc4d41832099fd6f6
Tu Vo <tuvo1106@gmail.com>
2019-03-18 22:12:04 +0000

    Refactor bigS.c

--- bigS.c
+++ bigS.c
@@ -21,56 +21,12 @@ int print_bigS(va_list l)
 			_puts("\\x");
 			count += 2;
 			res = convert(s[i], 16, 0);
-			if (_atoi(res) <= 16)
-			{
-				_putchar('0');
-				count++;
-			}
-			_printf(res);
-			count++;
+			if (!res[1])
+				count += _putchar('0');
+			count += _puts(res);
 		}
 		else
-			_putchar(s[i]);
+			count += _putchar(s[i]);
 	}
 	return (count);
-}
-
-/**
- * isDigit - returns true if i is a number
- * @i: integer i
- * Return: true if number, false if not
- */
-int isDigit(int i)
-{
-	return (i >= '0' && i <= '9');
-}
-
-/**
- * _atoi - converts a string to integer
- * @s: string s
- * Return: returns parsed integer
- */
-int _atoi(char *s)
-{
-	int res = 0;
-	int negs = 0;
-	int foundNumber = 0;
-	int multiplier = 1;
-
-	while (*s)
-	{
-		if (!isDigit(*s) && foundNumber)
-			break;
-		if (*s == '-')
-			negs++;
-		else if (isDigit(*s))
-		{
-			foundNumber = 1;
-			if (negs % 2)
-				multiplier = -1;
-			res = res * 10 + (*s - 48) * multiplier;
-		}
-		s++;
-	}
-	return (res);
 }
