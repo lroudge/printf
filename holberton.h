a7691094d991bd54348de4eb8404fa6f665cee15
lroudge <lauradrc@hotmail.fr>
2019-03-18 02:15:55 +0000

    Comment out other functions

--- holberton.h
+++ holberton.h
@@ -13,24 +13,25 @@
 typedef struct printHandler
 {
 	char c;
-	unsigned int (*f)(va_list ap);
+	int (*f)(va_list ap);
 } ph;
 
 int _printf(const char *fmt, ...);
-unsigned int (*get_print(char s))();
-unsigned int print_string(va_list l);
-unsigned int print_char(va_list l);
-unsigned int print_int(va_list l);
-void print_number(int n);
-unsigned int print_unsigned(va_list l);
-void print_unsigned_number(unsigned int n);
-unsigned int print_hex(va_list l);
-unsigned int print_hex_big(va_list l);
-unsigned int print_binary(va_list l);
-unsigned int print_octal(va_list l);
-char *convert(unsigned int num, int base, int lowercase);
-unsigned int _putchar(char c);
-unsigned int count_digit(int i);
-int _strcmp(const char *s1, const char *s2);
-
+int (*get_print(char s))(va_list);
+int print_string(va_list l);
+int print_char(va_list l);
+int _putchar(char c);
+/**
+ * int print_int(va_list l);
+ * void print_number(int n);
+ * int print_unsigned(va_list l);
+ * void print_unsigned_number(unsigned int n);
+ * int print_hex(va_list l);
+ * int print_octal(va_list l);
+ * int print_hex_big(va_list l);
+ * int print_binary(va_list l);
+ * char *convert(unsigned int num, int base, int lowercase);
+ * int count_digit(int i);
+ * int _strcmp(const char *s1, const char *s2);
+ */
 #endif
