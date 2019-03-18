e534e52655e7676272f967caf33bb2da50a105b9
Tu Vo <tuvo1106@gmail.com>
2019-03-18 02:58:02 +0000

    Add printing numbers functions

--- holberton.h
+++ holberton.h
@@ -16,7 +16,17 @@ typedef struct printHandler
 	int (*f)(va_list ap);
 } ph;
 
-int _printf(const char *format, ...);
+int print_int(va_list l);
+void print_number(int n);
+int print_unsigned(va_list l);
+void print_unsigned_number(unsigned int n);
+int print_hex(va_list l);
+int print_hex_big(va_list l);
+int print_binary(va_list l);
+int print_octal(va_list l);
+char *convert(unsigned int num, int base, int lowercase);
+int count_digit(int i);
+int _printf(const char *fmt, ...);
 int (*get_print(char s))(va_list);
 int print_string(va_list l);
 int print_char(va_list l);
