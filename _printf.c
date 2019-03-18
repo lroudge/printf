b4f759c08fa8a902095fbc286e58c957a79e1cb1
lroudge <lauradrc@hotmail.fr>
2019-03-18 02:39:47 +0000

    Change argument of va_start

--- _printf.c
+++ _printf.c
@@ -15,7 +15,7 @@ int _printf(const char *format, ...)
 	int count = 0;
 	va_list arguments;
 
-	va_start(arguments, fmt);
+	va_start(arguments, format);
 
 	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
