2a25e58330797afb73b19a6c786c32333961e593
Tu Vo <tuvo1106@gmail.com>
2019-03-19 15:02:24 +0000

    Removed unneccessary helper and tidied up header

--- converter.c
+++ converter.c
@@ -25,26 +25,3 @@ char *convert(unsigned long int num, int base, int lowercase)
 
 	return (ptr);
 }
-
-/**
- * convert_binary - converts number into binary and into string
- * @num: input number
- * @base: input base
- * Return: result string
- */
-char *convert_binary(unsigned int num, int base)
-{
-	static char *rep;
-	static char buffer[50];
-	char *ptr;
-
-	rep = "0123456789abcdef";
-	ptr = &buffer[49];
-	*ptr = '\0';
-	do {
-		*--ptr = rep[num % base];
-		num /= base;
-	} while (num != 0);
-
-	return (ptr);
-}
--- holberton.h
+++ holberton.h
@@ -16,22 +16,36 @@ typedef struct printHandler
 	int (*f)(va_list ap);
 } ph;
 
+/* print_nums */
 int print_int(va_list l);
 void print_number(int n);
 int print_unsigned(va_list l);
+int count_digit(int i);
+
+/* print_bases */
 int print_hex(va_list l);
 int print_hex_big(va_list l);
 int print_binary(va_list l);
 int print_octal(va_list l);
+
+/* converter */
 char *convert(unsigned long int num, int base, int lowercase);
-char *convert_binary(unsigned int num, int base);
-int count_digit(int i);
+
+/* _printf */
 int _printf(const char *fmt, ...);
+
+/* get_print */
 int (*get_print(char s))(va_list);
+
+/* print_alpha */
 int print_string(va_list l);
 int print_char(va_list l);
+
+/* write_funcs */
 int _putchar(char c);
 int _puts(char *str);
+
+/* print_custom */
 int print_rot13(va_list l);
 int print_rev(va_list l);
 int print_bigS(va_list l);
