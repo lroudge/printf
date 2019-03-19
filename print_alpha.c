73c3b8fc1793885b1242d1bce48dcdb6186d90b1
Tu Vo <tuvo1106@gmail.com>
2019-03-19 15:20:00 +0000

    Refactor folder structure

--- print_alpha.c
+++ print_alpha.c
@@ -25,17 +25,3 @@ int print_char(va_list l)
 	_putchar(va_arg(l, int));
 	return (1);
 }
-
-/**
- * _puts - prints a string to stdout
- * @str: pointer to the string to print
- * Return: number of chars written
- */
-int _puts(char *str)
-{
-	int i;
-
-	for (i = 0; str[i] != '\0'; i++)
-		_putchar(str[i]);
-	return (i);
-}
