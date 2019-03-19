be7759113b471637bb1e3e15907c138f79683e37
Tu Vo <tuvo1106@gmail.com>
2019-03-19 14:49:33 +0000

    Refactor _printf

--- _printf.c
+++ _printf.c
@@ -12,11 +12,11 @@ int _printf(const char *format, ...)
 {
 	int (*pfunc)(va_list);
 	const char *p;
-	int count = 0;
 	va_list arguments;
 
-	va_start(arguments, format);
+	register int count = 0;
 
+	va_start(arguments, format);
 	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
 	for (p = format; *p; p++)
@@ -26,8 +26,7 @@ int _printf(const char *format, ...)
 			p++;
 			if (*p == '%')
 			{
-				_putchar('%');
-				count++;
+				count += _putchar('%');
 				continue;
 			}
 			pfunc = get_print(*p);
@@ -35,10 +34,7 @@ int _printf(const char *format, ...)
 				? pfunc(arguments)
 				: _printf("%%%c", *p);
 		} else
-		{
-			count++;
-			_putchar(*p);
-		}
+			count += _putchar(*p);
 	}
 	_putchar(-1);
 	va_end(arguments);
