81f2553c032b16a229868e68ec2b4f7b4bd132ac
lroudge <lauradrc@hotmail.fr>
2019-03-19 18:42:10 +0000

    Check the errors at compilation

--- _printf.c
+++ _printf.c
@@ -10,7 +10,7 @@
  */
 int _printf(const char *format, ...)
 {
-	int (*pfunc)(va_list);
+	int (*pfunc)(va_list, flags_t *);
 	const char *p;
 	va_list arguments;
 	flags_t flags = {0, 0, 0};
@@ -34,7 +34,7 @@ int _printf(const char *format, ...)
 				p++;
 			pfunc = get_print(*p);
 			count += (pfunc)
-				? pfunc(arguments)
+				? pfunc(arguments, &flags)
 				: _printf("%%%c", *p);
 		} else
 			count += _putchar(*p);
